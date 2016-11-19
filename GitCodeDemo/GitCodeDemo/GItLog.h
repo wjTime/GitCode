2d998faedce56e21a8905c948e4aa09071dd6ac1
高文杰 <yidong-ios16@yhd.com>
2016-11-19 15:39:59 +0800

    增加log类

---
+++ GitCodeDemo/GitCodeDemo/GItLog.h
@@ -0,0 +1,13 @@
+//
+//  GItLog.h
+//  GitCodeDemo
+//
+//  Created by wenjie on 16/11/19.
+//  Copyright © 2016年 OTS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GItLog : NSObject
+
+@end
---
+++ GitCodeDemo/GitCodeDemo/GItLog.m
@@ -0,0 +1,13 @@
+//
+//  GItLog.m
+//  GitCodeDemo
+//
+//  Created by wenjie on 16/11/19.
+//  Copyright © 2016年 OTS. All rights reserved.
+//
+
+#import "GItLog.h"
+
+@implementation GItLog
+
+@end
